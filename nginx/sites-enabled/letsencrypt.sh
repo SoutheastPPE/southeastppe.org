9218ef1f9a94aad1f570c9221adab04d7c5df39c
Noah Williams <noah@southeastppe.org>
2020-03-28 22:39:24 -0800

    Change LetsEncrypt cert storage location

--- nginx/sites-enabled/letsencrypt.sh
+++ nginx/sites-enabled/letsencrypt.sh
@@ -1,6 +1,6 @@
 docker run -it --rm --name certbot \
-  -v /etc/letsencrypt:/etc/letsencrypt \
-  -v /etc/letsencrypt/log:/var/log/letsencrypt \
+  -v ./letsencrypt:/etc/letsencrypt \
+  -v ./letsencrypt/log:/var/log/letsencrypt \
   -p 80:80 \
   certbot/certbot -t certonly \
   --standalone \
